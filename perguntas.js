1e8ccdb315a654a4533fccdc14d5d140adeef95f
heloisa150642 <147078015+heloisa150642@users.noreply.github.com>
2024-09-20 10:53:25 -0300

    ...

--- perguntas.js
+++ perguntas.js
@@ -1,7 +1,7 @@
 criaCartao(
     'Programação',
-    'O que é Python?',
-    'O Python é uma linguagem de programação'
+    'O que é lógica de programação?',
+    'Construção do algoritmo'
 )
 
 criaCartao(
